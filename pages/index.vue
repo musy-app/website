898ca5c1fd29a13aa1bb5dbafaffb762bf26770f
Lucas <bellierlucas.pro@gmail.com>
2022-03-13 01:09:12 +0100

    🎉 initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Musy is a blind test app that is currently in development</h1>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style lang="scss">
+h1 {
+  text-align: center;
+}
+</style>

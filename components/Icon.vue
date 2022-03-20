31b57656e6067124f32b2b5665170cd7db9384ac
Lucas <bellierlucas.pro@gmail.com>
2022-03-20 20:06:12 +0100

    ✨ Introduce component Icon

---
+++ components/Icon.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="single-icon" :style="`width: ${width}px; height: ${height}px;`">
+    <component :is="icon" :color="color" />
+  </div>
+</template>
+
+<script>
+import DoubleArrowLeftRight from "./Icon/DoubleArrowLeftRight.vue";
+
+export default {
+  components: {
+    DoubleArrowLeftRight,
+  },
+  props: {
+    icon: {
+      type: String,
+      default: "",
+    },
+    color: {
+      type: String,
+      default: "#FFFFFF",
+    },
+    width: {
+      type: [Number, String],
+      default: 24,
+    },
+    height: {
+      type: [Number, String],
+      default: 24,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.single-icon {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  position: relative;
+  cursor: pointer;
+  padding: 0;
+  &:focus {
+    outline: none;
+  }
+  svg {
+    width: 100%;
+    padding: 0;
+    height: auto;
+  }
+}
+</style>
